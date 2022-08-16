5f4c423d18b09c2d569d4b41854e0948d0ae9336
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-08-16 22:26:26 +0530

    added import export buttons

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const add = document.querySelector('button');
-const div = document.querySelector('#container');
+const div = document.getElementById('container');
 const auto = 'auto ';
 let URL;
 let col;
 let grid = 0;
 
-add.addEventListener('click', () => {
+let add = () => {
 
   const a = document.createElement('a');
 
   URL = prompt('Enter URL');
 
-  if (URL != null) {
+  if (URL != '' && URL != null) {
 
     a.innerHTML = prompt('Enter Name');
 
@@ -23,7 +22,7 @@ add.addEventListener('click', () => {
 
     }
   }
-});
+}
 
 // increases decreases grid columns
 let gridFX = (val) => {
@@ -33,3 +32,4 @@ let gridFX = (val) => {
     div.style.gridTemplateColumns = auto.repeat(col + 1);
   }
 }
+
