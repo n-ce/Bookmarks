03f512d39bd677d9b1ce381837a53dbd03bd3ff6
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-08-16 21:25:27 +0530

    only reassign column value when column limit reached

--- script.js
+++ script.js
@@ -2,30 +2,34 @@ const add = document.querySelector('button');
 const div = document.querySelector('#container');
 const auto = 'auto ';
 let URL;
+let col;
 let grid = 0;
 
 add.addEventListener('click', () => {
 
   const a = document.createElement('a');
-  
+
   URL = prompt('Enter URL');
-  
+
   if (URL != null) {
-    
-    a.href = URL;
+
     a.innerHTML = prompt('Enter Name');
-    
+
     if (a.innerHTML != '') {
-      
+      a.href = URL;
       div.appendChild(a);
       gridFX(grid);
       grid++;
-   
+
     }
   }
 });
 
 // increases decreases grid columns
 let gridFX = (val) => {
-  div.style.gridTemplateColumns = auto.repeat(Math.floor(Math.sqrt(val)) + 1);
+  col = Math.sqrt(val);
+  // only run when column limit reached
+  if (col == Math.floor(col)) {
+    div.style.gridTemplateColumns = auto.repeat(col + 1);
+  }
 }
