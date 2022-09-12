f961f4d06b0e45d759d27af0258b0ea4316f257a
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-09-12 19:53:10 +0530

    initial functional update

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-const div = document.getElementById('container');
-const auto = 'auto ';
-let URL;
+const div = document.getElementsByTagName('section')[0];
+let URL = [];
 let col;
 let grid = 0;
 
-let add = () => {
-
+const add = () => {
   const a = document.createElement('a');
-
-  URL = prompt('Enter URL');
-
-  if (URL != '' && URL != null) {
-
-    a.innerHTML = prompt('Enter Name');
-
-    if (a.innerHTML != '') {
-      a.href = URL;
-      div.appendChild(a);
-      gridFX(grid);
-      grid++;
-
-    }
+  URL = prompt('Enter Name & URL separated by comma').split(',');
+  if (URL[1] != undefined && URL[1] != '') {
+    a.innerHTML = URL[0];
+    a.href = URL[1];
+    div.appendChild(a);
+    gridFX(grid);
+    grid++;
   }
 }
 
+const cssVar = (p, q) => { document.querySelector(':root').style.setProperty(p, q); }
 // increases decreases grid columns
-let gridFX = (val) => {
+const gridFX = (val) => {
   col = Math.sqrt(val);
   // only run when column limit reached
-  if (col == Math.floor(col)) {
-    div.style.gridTemplateColumns = auto.repeat(col + 1);
+  if (col == Math.floor(col) && col < 7) {
+    cssVar('--h1', (85 - (15 * col)) + '%');
+    cssVar('--div', (15 + (15 * col)) + '%');
+    cssVar('--fontSize', ((3 - (0.4 * col)) / 1.5) + 'rem');
+    div.style.gridTemplateColumns = 'auto '.repeat(col + 1);
   }
 }
-
