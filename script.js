0b92c98d1af3cf898ff46b7095ef93a570cbcbe9
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-09-13 21:36:01 +0530

    added remove function albeit not fully stable

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const div = document.getElementsByTagName('section')[0];
+const removers = document.getElementsByTagName('p');
 let URL = [];
 let col;
 let grid = 0;
 
 const add = () => {
-  const a = document.createElement('a');
   URL = prompt('Enter Name & URL separated by comma').split(',');
+  
   if (URL[1] != undefined && URL[1] != '') {
-    a.innerHTML = URL[0];
-    a.href = URL[1];
-    div.appendChild(a);
-    gridFX(grid);
-    grid++;
+    
+    div.innerHTML += `<span><a href=${URL[1]}>${URL[0]}</a><p class="gg-close-o"></p></span>`;
+    gridFX(grid++);
+    
+    if (removers[0].classList.contains('display'))
+      removers[removers.length - 1].classList.add('display');
   }
 }
-
 const cssVar = (p, q) => { document.querySelector(':root').style.setProperty(p, q); }
 // increases decreases grid columns
 const gridFX = (val) => {
@@ -25,5 +26,15 @@ const gridFX = (val) => {
     cssVar('--div', (15 + (15 * col)) + '%');
     cssVar('--fontSize', ((3 - (0.4 * col)) / 1.5) + 'rem');
     div.style.gridTemplateColumns = 'auto '.repeat(col + 1);
+  }
+}
+
+const closer = () => {
+  for (k of removers) {
+    k.classList.toggle('display');
+    k.addEventListener('click', () => {
+      k.parentElement.remove();
+      gridFX(grid--);
+    });
   }
 }
