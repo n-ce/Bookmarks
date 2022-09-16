67e11e9e9e09b08e05123eef42bfa7d3c88b6b53
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-09-16 22:26:08 +0530

    UI simplified

--- script.js
+++ script.js
@@ -9,21 +9,6 @@ let obj;
 
 const cssVar = (p, q) => document.querySelector(':root').style.setProperty(p, q);
 
-// increases decreases grid columns
-const gridChange = (no) => {
-  col = Math.sqrt(no);
-
-  // only run when col is an absolute value
-
-  if (col == Math.floor(col) && col < 7) {
-    cssVar('--top', (85 - (15 * col)) + '%');
-    cssVar('--bottom', (15 + (15 * col)) + '%');
-    cssVar('--fontSize', ((3 - (0.4 * col)) / 1.5) + 'rem');
-    linksParent.style.gridTemplateColumns = 'auto '.repeat(col + 1);
-  }
-}
-
-
 
 const add = () => {
   nameLinks = prompt('Enter Name & URL separated by comma').split(',');
@@ -41,32 +26,25 @@ const add = () => {
         n++;
     });
     if (n == arr.length) {
-      linksParent.innerHTML += `<span><a href=${obj.URL}>${obj.Name}</a><p class="gg-close-o"></p></span>`;
+      linksParent.innerHTML += `<span><a href=${obj.URL}>${obj.Name}</a><p>×</p></span>`;
       arr.push(obj);
     }
     localStorage.setItem('data', JSON.stringify(arr));
     //
-    gridChange(gridNo++);
 
     if (linksRemovers[0].classList.contains('display'))
       linksRemovers[linksRemovers.length - 1].classList.add('display');
   }
 }
 
-const closer = () => {
+const remove = () => {
   for (k of linksRemovers) {
     k.classList.toggle('display');
     k.addEventListener('click', () => {
       k.parentElement.remove();
-      gridChange(gridNo--);
     });
   }
 }
-
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-  for (let i = 1; i < 5; i++)
-    document.getElementsByTagName('button')[i].classList.toggle('hide');
-})
 
 
 let Export = () => {
@@ -90,7 +68,7 @@ let Import = (val) => {
   arr = arr.concat(JSON.parse(val));
   linksParent.innerHTML = null;
   for (const x of arr)
-    linksParent.innerHTML += `<span><a href=${x.URL}>${x.Name}</a><p class="gg-close-o"></p></span>`;
+    linksParent.innerHTML += `<span><a href=${x.URL}>${x.Name}</a><p>×</p></span>`;
 }
 
 if (localStorage.getItem('data') != null) {
